ffc15dffea8811d852ed53168b2f1ab8352b61c2
Gowii3K <rgadithya997@gmail.com>
2025-03-02 20:27:25 +0530

    feat: enabled cors
    -enable requests from react and react native to backend

--- match-verse-server/src/main.ts
+++ match-verse-server/src/main.ts
@@ -7,6 +7,13 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:8081'],
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
