be1b38b2583ec95b8641402373fb21ed5f98db18
Gowii3K <rgadithya997@gmail.com>
2025-03-10 12:26:16 +0530

    feat: added unit tests for booking controller

--- match-verse-server/src/bookings/bookings.service.spec.ts
+++ match-verse-server/src/bookings/bookings.service.spec.ts
@@ -1,13 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookingsService } from './bookings.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('BookingsService', () => {
   let service: BookingsService;
+  const mockBookingService = {};
+
+  const mockPrismaService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BookingsService],
-    }).compile();
+      providers: [BookingsService, PrismaService],
+    })
+      .overrideProvider(BookingsService)
+      .useValue(mockBookingService)
+      .overrideProvider(PrismaService)
+      .useValue(mockPrismaService)
+      .compile();
 
     service = module.get<BookingsService>(BookingsService);
   });
--- match-verse-server/src/bookings/bookings.service.ts
+++ match-verse-server/src/bookings/bookings.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { Slot } from './interface/slot';
 import { UserBookingDto } from './dto/user-booking.dto';
 import { VenueBookingDto } from './dto/venue-booking.dto';
