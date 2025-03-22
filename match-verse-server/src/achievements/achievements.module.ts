55cfdeededd48b76141ebdb802e4429620076a93
Hactivating <159514668+Hactivating@users.noreply.github.com>
2025-03-22 23:43:36 +0530

    feature / fixed modules files

--- match-verse-server/src/achievements/achievements.module.ts
+++ match-verse-server/src/achievements/achievements.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AchievementsService } from './achievements.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [AchievementsService]
+  providers: [AchievementsService, PrismaService],
+  imports: [PrismaModule],
+  exports: [AchievementsService],
+
 })
-export class AchievementsModule {}
+export class AchievementsModule { }
--- match-verse-server/src/app.module.ts
+++ match-verse-server/src/app.module.ts
@@ -8,19 +8,16 @@ import { CourtsModule } from './courts/courts.module';
 import { BookingsModule } from './bookings/bookings.module';
 import { UsersModule } from './users/users.module';
 import { LeaderboardModule } from './leaderboard/leaderboard.module';
-
 import { MatchModule } from './match/match.module';
 import { S3Module } from './s3/s3.module';
 import { RankingModule } from './ranking/ranking.module';
 import { MatchResultModule } from './match-result/match-result.module';
-import { AchivementsModule } from './achivements/achivements.module';
-import { AchievementsController } from './achievements/achievements.controller';
 import { AchievementsModule } from './achievements/achievements.module';
 
 
 @Module({
-  imports: [PrismaModule, VenuesModule, AuthModule, CourtsModule, BookingsModule, UsersModule, MatchModule,S3Module,LeaderboardModule,MatchResultModule,RankingModule, AchivementsModule, AchievementsModule],
-  controllers: [AppController, AchievementsController],
+  imports: [PrismaModule, VenuesModule, AuthModule, CourtsModule, BookingsModule, UsersModule, MatchModule, S3Module, LeaderboardModule, MatchResultModule, RankingModule, AchievementsModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
