f27ea258c9d91eaa946670dc49ff9b6ec8533ccb
Hactivating <159514668+Hactivating@users.noreply.github.com>
2025-03-23 11:05:52 +0530

    updated migrations

--- match-verse-server/src/match-result/match-result.module.ts
+++ match-verse-server/src/match-result/match-result.module.ts
@@ -3,10 +3,11 @@ import { MatchResultService } from './match-result.service';
 import { MatchResultController } from './match-result.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { MatchService } from 'src/match/match.service';
+import { RankingService } from 'src/ranking/ranking.service';
 
 @Module({
   controllers: [MatchResultController],
-  providers: [MatchResultService, PrismaService, MatchService],
+  providers: [MatchResultService, PrismaService, MatchService, RankingService],
   exports: [MatchResultService]
 })
-export class MatchResultModule {}
+export class MatchResultModule { }
